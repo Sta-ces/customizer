f7a37d12a39d2071ca62f6f1ae857a75d3eb3066
Cedric Staces <34098638+Sta-ces@users.noreply.github.com>
2026-07-08 09:12:47 +0200

    Update customizer.php

--- customizer.php
+++ customizer.php
@@ -11,14 +11,14 @@
 
 namespace StacesBuilder\Inc\Customizer;
 
+use StacesBuilder\Inc\Customizer\ST_Slider_Control;
+
 if (!defined('ABSPATH')) { exit; }
 
 // Plugin activation
 register_activation_hook(__FILE__, function(){ update_option('stacesbuilder_customizer_activated', true); });
 // Deactivating the plugin
 register_deactivation_hook(__FILE__, function(){ delete_option('stacesbuilder_customizer_activated'); });
-
-use StacesBuilder\Inc\Customizer\ST_Slider_Control;
 
 require_once(ABSPATH . 'wp-admin/includes/plugin.php');
 require_once(__DIR__ . '/config/class-slider-control.php');
